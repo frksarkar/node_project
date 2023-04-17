dee6a4ba430321a6362f3c9361e20fbaa1b74d1c
faruksarkar <hazrat.omurfaruk@gmail.com>
2023-04-17 21:38:14 +0600

    add handler token and check

--- practice_project_02/routes.js
+++ practice_project_02/routes.js
@@ -8,10 +8,14 @@
 // dependencies
 const { sampleHandler } = require('./handler/routeHandlers/sampleHandler');
 const { userHandler } = require('./handler/routeHandlers/userHandler');
+const { tokenHandler } = require('./handler/routeHandlers/tokenHandler');
+const { checkHandler } = require('./handler/routeHandlers/checkHandler');
 
 const routes = {
     sample: sampleHandler,
     user: userHandler,
+    token: tokenHandler,
+    check: checkHandler,
 };
 
 module.exports = routes;
