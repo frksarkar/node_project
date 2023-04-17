a7f701555449fb8de158cb7cc57c2a1a97668237
faruksarkar <hazrat.omurfaruk@gmail.com>
2023-04-18 02:21:18 +0600

    add

--- practice_project_01/lib/math.js
+++
@@ -1,9 +0,0 @@
-const math = {};
-
-math.getRandomNumber = function getRandomNumber(min, max) {
-    const minimum = typeof min === 'number' ? min : 0;
-    const maximum = typeof max === 'number' ? max : 0;
-    return Math.floor(Math.random() * (maximum - minimum + 1) + minimum);
-};
-
-module.exports = math;
--- practice_project_01/network/network.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require('../index');
-
-http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write(`${app.printAQuotes()}`);
-        res.end();
-    }
-}).listen(3000);
