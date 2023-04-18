140f13f73b4f8b8f01a55d644c59b961e9276eb1
faruksarkar <hazrat.omurfaruk@gmail.com>
2023-04-18 16:33:57 +0600

    complete notification boilerplate

---
+++ practice_project_02/helpers/notification.js
@@ -0,0 +1,18 @@
+/**
+ * *Title: notifications library
+ * ?description: important function to notify users
+ * @author: faruk sarkar
+ * *Date: 18-April-2023
+ */
+
+// dependencies
+const https = require('https');
+
+// module scaffolding
+const notifications = {};
+
+// send sms to user using twilio api
+notifications.sendSMS = (phone, msg, callback) => {};
+
+// export the module
+module.exports = notifications;
