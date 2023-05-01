79ce7fa78d7c3f491b0317212d1f4c1787a79d62
omar faruk sarkar <hazrat.omurfaruk@gmail.com>
2023-05-01 12:29:59 +0600

    remove test file

--- uptime monitoring api/test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-// eslint-disable-next-line consistent-return
-fs.open('example.txt', 'r+', (err, fd) => {
-    console.log(fd);
-    if (err) {
-        return console.error(err);
-    }
-});
